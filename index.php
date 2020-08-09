9fec0bdfa69d41b8adde48ec8314959c61671237
Daothu <doanphong.1413@gmail.com>
2020-08-09 16:21:01 +0700

    add something else

--- index.php
+++ index.php
@@ -1,30 +1,79 @@
 <?php include("includes/header.php"); ?>
+<?php
+$page = !empty($_GET['page']) ? $_GET['page'] : 1;
+$items_per_page = 4;
+$items_total_count =  Photo::count_all();
+$paginate = new Paginate($page, $items_per_page, $items_total_count);
+$sql = "SELECT * FROM  photo ";
+$sql .= "LIMIT {$items_per_page} ";
+$sql .= "OFFSET {$paginate->offset()} ";
+$photos = Photo::find_by_query($sql);
+?>
 
+<div class="row">
+    <div class="thumbnails row">
+        <!-- Blog Entries Column -->
+        <div class="col-md-12">
+            <?php
+            foreach ($photos as $photo) {
+            ?>
 
+                <div class="col-xs-6 col-md-3">
+                    <a href="photo.php?id=<?php echo $photo->id ?>" class="thumbnail">
+                        <img class="home_page_photo img-responsive" src="admin/<?php echo $photo->picture_path() ?>" alt="">
+                    </a>
+                </div>
+
+            <?php
+            }
+
+            ?>
+
+        </div>
         <div class="row">
 
-            <!-- Blog Entries Column -->
-            <div class="col-md-8">
 
-    
-            
-          
-         
+            <ul class="pager">
 
-            </div>
+                <?php
 
 
+                if ($paginate->page_total() > 1) {
+                    if ($paginate->has_next()) {
+                        echo "<li class='next'><a href='index.php?page={$paginate->next()}'>Next</a></li>";
+                    }
+                    for ($i = 1; $i <= $paginate->page_total(); $i++) {
+                        if ($i == $paginate->current_page) {
+                            echo  "<li class='active'><a href='index.php?page={$i}'>{$i}</a></li>";
+                        } else {
+                            echo  "<li><a href='index.php?page={$i}'>{$i}</a></li>";
+                        }
+                    }
+                    if ($paginate->has_previous()) {
+
+                        echo "<li class='previous'><a href='index.php?page={$paginate->previous()}'>Previous</a></li>";
+                    }
+                }
 
 
-            <!-- Blog Sidebar Widgets Column -->
-            <div class="col-md-4">
+                ?>
 
-            
-                 <?php include("includes/sidebar.php"); ?>
+
+            </ul>
+
+
+
+
 
 
 
         </div>
-        <!-- /.row -->
 
-        <?php include("includes/footer.php"); ?>
+    </div>
+
+
+    <!-- Blog Sidebar Widgets Column -->
+
+    <!-- /.row -->
+
+    <?php include("includes/footer.php"); ?>
